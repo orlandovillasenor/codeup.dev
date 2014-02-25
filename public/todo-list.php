ad5cc2c7839e099f0a5ad570bf637e84a5d7af07
Orlando Villaseñor <orlandovillasenor@me.com>
2014-02-25 08:59:03 -0600

    supe rglobals
    For this exercise, you will be converting the todo-list.html template
    you created into a PHP driven web app. Commit and push your changes to
    GitHub after each step.
    
    Change the file extension of your todo list template to .php.
    
    Create an array from your sample todo list items in the template. Next,
    use PHP to display the array items within the unordered list in your
    template and test in your browser.
    
    Reference the code you wrote in your command line todo list app to add
    the ability to load todo items from a file. The items should be loaded
    into an array, and then that array should be used to display the items
    just as in the above steps.
    
    Using the POST method on the form in your template, create the ability
    to add todo items to the list. Each time an item is added, the todo
    list file should be saved with the new item added.
    
    Add a link next to each todo item that says "Mark Complete" and have it
    send a GET request to the page that deletes the entry. Use query
    strings to send the proper key back to the server, and update the todo
    list file to reflect the deletion.

--- public/todo-list.php
+++ public/todo-list.php
@@ -1,14 +1,3 @@
-<?php 
-
-echo "<p>POST:</p>";
-var_dump($_POST);
-
-echo "<p>GET:</p>";
-var_dump($_GET);
-
-?>
-
-
 
 <!DOCTYPE html>
 <html>
@@ -16,22 +5,77 @@ var_dump($_GET);
 		<title>TODO List</title>
 	</head>
 	<body>
-		<h1>TODO List:</h1>
-			<ul>
-				<li>Wake up</li>
-				<li>Work out</li>
-				<li>Go to Codeup</li>
-				<li>Learn to code</li>
-			</ul>
-		<h2>Add Item to List:</h2>
-			<form method="GET" action="">
+		<h1>My List:</h1>
+		<ul>
+			<?php 
+				function open_file($filename){
+				    $handle = fopen($filename, "r");
+				    $contents = fread($handle, filesize($filename));
+				    $contents_array = explode("\n", $contents);
+				    return $contents_array;
+				    fclose($handle);
+				}
+
+				function save_file($filename, $array){
+				    $handle = fopen($filename, 'w');
+				    $string = implode("\n", $array);
+				    return fwrite($handle, $string);
+				    fclose($handle);
+				}
+				
+				if (filesize('data/todo-list.txt') > 0) {
+					$items = open_file('data/todo-list.txt');
+				} else {
+					$items = array();
+				}
+		
+				if (isset($_POST['item']) && !empty($_POST)) {
+						
+						$new_item = $_POST['item'];
+						$new_item = ucwords($new_item);
+						array_push($items, $new_item);					
+						save_file("data/todo-list.txt", $items);
+					}
+				
+				if (isset($_GET['remove'])) {
+						$itemsId = $_GET['remove'];
+						unset($items[$itemsId]);
+						save_file("data/todo-list.txt", $items);
+						header("Location: todo-list.php");
+						exit(0);
+					}
+				
+				foreach ($items as $key => $item) {
+					echo "<li>$item <a href=\"?remove=$key\">Remove Item</a> </li>";
+				}
+			
+				//var_dump($_POST);
+			?>
+		</ul>
+		
+		<!-- <h3>Add Item to List</h3> -->
+			<form method="POST" action="">
 				<p>
-				<label for="add">Enter Item:</label>
-				<input id="add" name="add" type="text">
+				<label for="item">Enter Item:</label>
+				<input id="item" name="item" type="text" autofocus>
 				</p>
 				<p>
 					<button type="submit">Add to list</button>
 				</p>
 			</form>
+			
+
+
 	</body>
 </html>
+				
+
+				
+
+			
+
+
+
+			
+			
+			    
