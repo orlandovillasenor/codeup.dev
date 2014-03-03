8c8219802cfb43baf7b7391f3f69f27d9efc957a
Orlando Villaseñor <orlandovillasenor@me.com>
2014-03-03 14:17:52 -0600

    added throw exceptions

--- public/todo-list.php
+++ public/todo-list.php
@@ -1,35 +1,32 @@
 <?php
-//var_dump($_FILES);
-function open_file($filename){
-    $handle = fopen($filename, "r");
-    $contents = fread($handle, filesize($filename));
-    $contents_array = explode("\n", $contents);
-    return $contents_array;
-    fclose($handle);
-}
-function save_file($filename, $array){
-    $handle = fopen($filename, 'w');
-    $string = implode("\n", $array);
-    fwrite($handle, $string);
-    fclose($handle);
+require_once('classes/filestore.php');
+
+//$cleared_items = array();
+$todo_list = new Filestore ('data/todo-list.txt');
+
+if (filesize('data/todo-list.txt') > 0) {
+	//$items = $todo_list->read_lines();
+	$items = $todo_list->read();
+} else {
+	$items = array();
 }
 
-// if (filesize('data/todo-list.txt') > 0) {
-// 	$items = open_file('data/todo-list.txt');
-// } else {
-// 	$items = array();
-// }
-// ternary operator for if/else above
-$items = (filesize('data/todo-list.txt') > 0) ? open_file('data/todo-list.txt') : array();
-
+if (isset($_POST['item']) && strlen($_POST['item']) > 240) {
+	throw new Exception ("item entered is greater than 240 characters");
+}
 
 if (isset($_POST['item']) && !empty($_POST['item'])) {
-		
 	$new_item = $_POST['item'];
 	$new_item = ucwords($new_item);
 	array_push($items, $new_item);					
-	save_file("data/todo-list.txt", $items);
-}
+	//$todo_list->write_lines($items);
+	$todo_list->write($items);
+} else if (isset($_POST['item']) && empty($_POST['item'])) {
+ 		throw new Exception("item entered is empty.");
+} 
+
+
+		
 $invalid_file_type = FALSE;
 
 if (count($_FILES) > 0 && $_FILES['uploaded_file']['type'] != 'text/plain') {
@@ -38,24 +35,31 @@ if (count($_FILES) > 0 && $_FILES['uploaded_file']['type'] != 'text/plain') {
     // Set the destination directory for uploads
     $upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
     // Grab the filename from the uploaded file by using basename
-    $filename = basename($_FILES['uploaded_file']['name']);
+    $uploaded_filename = basename($_FILES['uploaded_file']['name']);
     // Create the saved filename using the file's original name and our upload directory
-    $saved_filename = $upload_dir . $filename;
+    $saved_filename = $upload_dir . $uploaded_filename;
     // Move the file from the temp location to our uploads directory
     move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $saved_filename);
-    $file_push = open_file($saved_filename);
-    foreach ($file_push as $value) {
-        $value = ucwords($value);
-        array_push($items, $value);
-        save_file("data/todo-list.txt", $items);
+    $upload = new Filestore($saved_filename);
+    $uploaded_file = $upload->read($saved_filename);
+    //$uploaded_file = $todo_list->read($saved_filename); 	
+	if (isset($_POST['replace_file'])) {
+    	unset($items);
+    	$items = $uploaded_file;
+    	$todo_list->write($items);
+    } else {
+    	foreach ($uploaded_file as $value) {
+    		//$value = ucwords($value);
+    		array_push($items, $value);
+ 	        $todo_list->write($items);		
+    	}
     }
 }
-
 
 if (isset($_GET['remove'])) {
 	$itemsId = $_GET['remove'];
 	unset($items[$itemsId]);
-	save_file("data/todo-list.txt", $items);
+	$todo_list->write($items);
 	header("Location: todo-list.php");
 	exit(0);
 }
@@ -67,41 +71,46 @@ if (isset($_GET['remove'])) {
 	<title>TODO List</title>
 </head>
 <body>
-<h1>My List:</h1>
-<ul>
+	<h1>My List:</h1>
+	<? if (count($items) > 0) : ?>
+	<ul>
+		<? foreach ($items as $key => $item) : ?>
+			<li><?= htmlspecialchars(strip_tags($item)); ?> <a href="?remove=<?= $key; ?>" >remove</a></li>
+		<? endforeach; ?>
+	</ul>
+	<? else : ?>
+		<p>Your list is empty</p>
+	<? endif; ?>
+	<h2>Add Item:</h2>
 
-	<? foreach ($items as $key => $item) : ?>
-		<li><?= htmlspecialchars(strip_tags($item)); ?> <a href="?remove=<?=$key;?>" >remove</a></li>
-	<? endforeach; ?>
+	<form method="POST" action="">
+		<p>
+			<label for="item">Enter Item:</label>
+			<input id="item" name="item" type="text" autofocus>
+		</p>
+		<p>
+			<button type="submit">Add to list</button>
+		</p>
+	</form>	
 
-</ul>
-<h2>Add Item:</h2>
+	<? if ($invalid_file_type == TRUE) : ?>
+		<p><?= "An invalid file type was uploaded"; ?></p>
+	<? endif; ?>
 
-<form method="POST" action="">
-	<p>
-		<label for="item">Enter Item:</label>
-		<input id="item" name="item" type="text" autofocus>
-	</p>
-	<p>
-		<button type="submit">Add to list</button>
-	</p>
-</form>	
+	<h2>Upload File:</h2>
 
-<? if ($invalid_file_type == TRUE) : ?>
-	<p><?= "An invalid file type was uploaded"; ?></p>
-<? endif; ?>
-
-<h2>Upload File:</h2>
-
-<form method="POST" enctype="multipart/form-data" action="">
-	<p>
-		<label for="uploaded_file">File to add to list</label>
-		<input id="uploaded_file" name="uploaded_file" type="file">
-	</p>
-	<p>
-		<button type="submit">Upload</button>
-	</p>
-</form>
+	<form method="POST" enctype="multipart/form-data" action="">
+		<p>
+			<label for="uploaded_file">File to add to list</label>
+			<input id="uploaded_file" name="uploaded_file" type="file">
+		</p>
+		<p>
+			<button type="submit">Upload</button>
+			<input id="replace_file" name="replace_file" type="checkbox">
+			<label for="replace_file">Replace existing list</label>
+			
+		</p>
+	</form>
 
 </body>
 </html>
